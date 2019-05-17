97365f8e5f87f10c02483eacde5cf5abef055827
刘辰曦 Bella <48339943+EbBella@users.noreply.github.com>
2019-05-17 11:09:38 +0800

    Create Practice 01.java

---
+++ Practice 01.java
@@ -0,0 +1,26 @@
+public static String weekday(int y){
+        if(y==1){
+            return "Monday";
+        }
+        else if(y==2){
+            return "Tuesday";
+        }
+        else if(y==3){
+            return "Wednesday";
+        }
+        else if(y==4){
+            return "Thursday";
+        }
+        else if(y==5){
+            return "Friday";
+        }
+        else if(y==6){
+            return "Saturday";
+        }
+        else if(y==7){
+            return "Sunday";
+        }
+        else{
+            return "Are you stupid??? HAHAHAHAHA~";
+        }
+    }
